1149b40454d4989900401ddae7f40e58c7abe4da
Skye Kim <kimmyb2uty@gmail.com>
2023-12-10 05:49:48 +0900

    javascript practice -> variable - if (연산자)

---
+++ javascript practice.js
@@ -0,0 +1,26 @@
+// 데이터 타입, 연산자 실습
+
+1+1
+// 2
+1+"만원"
+// '1만원'
+1+"1"
+// '11'
+1-"1"
+// 0
+"코드"+"캠프"
+// '코드캠프'
+"123" == 123
+// true
+"123" === 123
+// false
+true && true
+// true
+true && false
+// false
+false || true
+// true
+!false
+// true
+!true
+// false
