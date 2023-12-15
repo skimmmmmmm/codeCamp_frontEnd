4fa19a10ee1980593392adadd0a4dacf1bdfa225
Skye Kim <kimmyb2uty@gmail.com>
2023-12-15 22:59:47 +0900

    practice07 - javascript - mathematical objects

---
+++ understanding_javascript/practice-07-mathamaticalObjects.js
@@ -0,0 +1,22 @@
+Math.floor( Math.random() * 1000000 )
+// 158398
+Math.floor( Math.random() * 1000000 )
+// 414291
+Math.floor( Math.random() * 1000000 )
+// 44600
+Math.floor( Math.random() * 1000000 )
+// 995024
+Math.floor( Math.random() * 1000000 )
+// 502333
+Math.floor( Math.random() * 1000000 )
+// 862747
+String ( Math.floor( Math.random() * 1000000 )).padStart(6,"0")
+'417496'
+let result = String ( Math.floor( Math.random() * 1000000 )).padStart(6,"0")
+// undefined
+result
+// '863825'
+result
+// '863825'
+result
+// '863825'
